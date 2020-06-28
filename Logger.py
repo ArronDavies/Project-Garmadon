47e525d75e51241fe51e25eb97adc157beaddee0
encry <55758076+Jettford@users.noreply.github.com>
2020-06-28 21:57:47 +0100

    fixed

--- Logger.py
+++ Logger.py
@@ -4,7 +4,6 @@ import os
 import shutil
 from datetime import datetime
 from pathlib import Path
-import psutil
 import os
 
 config = configparser.ConfigParser()
@@ -76,92 +75,23 @@ def chatlogmanage():
 
 
 class LOGGINGLEVEL:
-	try:
-		ppid = os.getppid()  # Find parent process pid
-		value = psutil.Process(ppid).name()  # Get exe name of that process
-	except:  # Just so that using this on Linux and Mac doesn't break it
-		value = "noansi"
-	if value == "cmd.exe":
-		WARNING = '[WARNING]'
-		ERROR = '[ERROR]'
-		DEBUG = '[DEBUG]'
-		AUTHDEBUG = '[DEBUG][AUTH]'
-		WORLDDEBUG = '[DEBUG][WORLD]'
-		WORLDDEBUGROUTE = '[WORLD][ROUTE]'
-		REPLICADEBUG = '[DEBUG][REPLICA]'
-		CHARACTERDEBUG = '[DEBUG][CHARACTER]'
+	WARNING = '\u001b[33m[WARNING]'
+	ERROR = '\u001b[31m[ERROR]'
+	DEBUG = '\u001b[34m[DEBUG]'
+	AUTHDEBUG = '\u001b[32m[DEBUG][AUTH]'
+	WORLDDEBUG = '\u001b[34m[DEBUG][WORLD]'
+	WORLDDEBUGROUTE = '\u001b[34m[WORLD][ROUTE]'
+	REPLICADEBUG = '\u001b[34m[DEBUG][REPLICA]'
+	CHARACTERDEBUG = '\u001b[30m[DEBUG][CHARACTER]'
 
-		AUTH = '[AUTH]'
-		WORLD = '[WORLD]'
-		REPLICA = '[REPLICA]'
-		CHARACTER = '[CHARACTER]'
-		WEBSERVER = '[WEBSERVER]'
-		CLI = '[CLI]'
+	AUTH = '\u001b[32m[AUTH]'
+	WORLD = '\u001b[34m[WORLD]'
+	REPLICA = '\u001b[34m[REPLICA]'
+	CHARACTER = '\u001b[30m[CHARACTER]'
+	WEBSERVER = '\u001b[34m[WEBSERVER]'
+	CLI = '\u001b[34m[CLI]'
 
-		GAMEMESSAGE = '[GAMEMESSAGE]'
-		PACKET = ''
-		MESSAGE = ''
-		INFO = '[INFO]'
-	elif value == "powershell.exe":
-		WARNING = '[WARNING]'
-		ERROR = '[ERROR]'
-		DEBUG = '[DEBUG]'
-		AUTHDEBUG = '[DEBUG][AUTH]'
-		WORLDDEBUG = '[DEBUG][WORLD]'
-		WORLDDEBUGROUTE = '[WORLD][ROUTE]'
-		REPLICADEBUG = '[DEBUG][REPLICA]'
-		CHARACTERDEBUG = '[DEBUG][CHARACTER]'
-
-		AUTH = '[AUTH]'
-		WORLD = '[WORLD]'
-		REPLICA = '[REPLICA]'
-		CHARACTER = '[CHARACTER]'
-		WEBSERVER = '[WEBSERVER]'
-		CLI = '[CLI]'
-
-		GAMEMESSAGE = '[GAMEMESSAGE]'
-		PACKET = ''
-		MESSAGE = ''
-		INFO = '[INFO]'
-	elif value == "noansi":
-		WARNING = '[WARNING]'
-		ERROR = '[ERROR]'
-		DEBUG = '[DEBUG]'
-		AUTHDEBUG = '[DEBUG][AUTH]'
-		WORLDDEBUG = '[DEBUG][WORLD]'
-		WORLDDEBUGROUTE = '[WORLD][ROUTE]'
-		REPLICADEBUG = '[DEBUG][REPLICA]'
-		CHARACTERDEBUG = '[DEBUG][CHARACTER]'
-
-		AUTH = '[AUTH]'
-		WORLD = '[WORLD]'
-		REPLICA = '[REPLICA]'
-		CHARACTER = '[CHARACTER]'
-		WEBSERVER = '[WEBSERVER]'
-		CLI = '[CLI]'
-
-		GAMEMESSAGE = '[GAMEMESSAGE]'
-		PACKET = ''
-		MESSAGE = ''
-		INFO = '[INFO]'
-	else:
-		WARNING = '\u001b[33m[WARNING]'
-		ERROR = '\u001b[31m[ERROR]'
-		DEBUG = '\u001b[34m[DEBUG]'
-		AUTHDEBUG = '\u001b[32m[DEBUG][AUTH]'
-		WORLDDEBUG = '\u001b[34m[DEBUG][WORLD]'
-		WORLDDEBUGROUTE = '\u001b[34m[WORLD][ROUTE]'
-		REPLICADEBUG = '\u001b[34m[DEBUG][REPLICA]'
-		CHARACTERDEBUG = '\u001b[30m[DEBUG][CHARACTER]'
-
-		AUTH = '\u001b[32m[AUTH]'
-		WORLD = '\u001b[34m[WORLD]'
-		REPLICA = '\u001b[34m[REPLICA]'
-		CHARACTER = '\u001b[30m[CHARACTER]'
-		WEBSERVER = '\u001b[34m[WEBSERVER]'
-		CLI = '\u001b[34m[CLI]'
-
-		GAMEMESSAGE = '\u001b[36m[GAMEMESSAGE]'
-		PACKET = '\u001b[37m'
-		MESSAGE = '\u001b[35;1m'
-		INFO = '\u001b[36m[INFO]'
+	GAMEMESSAGE = '\u001b[36m[GAMEMESSAGE]'
+	PACKET = '\u001b[37m'
+	MESSAGE = '\u001b[35;1m'
+	INFO = '\u001b[36m[INFO]'
